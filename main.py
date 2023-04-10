bc973f5f4bdea78f30b2a1d0ed1bf04d9a65c462
YUEZIWU <yw3054@columbia.edu>
2023-04-10 15:56:29 -0400

    quick clean 0410

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from capacity_model import create_capacity_model
 from operation_model import create_operation_model
-from fixed_load_model import create_fix_load_model
+# from fixed_load_model import create_fix_load_model
 from utils import get_args
 import datetime
 
@@ -11,17 +11,11 @@ if __name__ == '__main__':
 
     fixed_load_bi = False
 
-    # 1 as stand alone, 2 as connected
-    config_list = [1,2]
-    for config in [2]:
-        if fixed_load_bi:
-            scenario_name = "Paloga-" + str(config) + '-fixed-load'
-            create_fix_load_model(args, config)
-        else:
-            scenario_name = "Test-" + str(config) + '-20230406-2'
-            nodes_capacity_results = create_capacity_model(args, config)
-            create_operation_model(args, nodes_capacity_results, scenario_name, config)
-
+    config_list = [1,2] # 1 as stand alone, 2 as connected
+    for config in config_list:
+        scenario_name = "Test-" + str(config) + '-20230406-2'
+        nodes_capacity_results = create_capacity_model(args, config)
+        create_operation_model(args, nodes_capacity_results, scenario_name, config)
 
     running_end_time = datetime.datetime.now()
     print(running_end_time - running_start_time)
